f16c1dbfdb2500a3689e9f0b550e5664d6969cfe
Nabfam <450767@bsd48.org>
2018-07-20 14:40:51 -0700

    Create Main.ino

---
+++ Main.ino
@@ -0,0 +1,17 @@
+
+//
+
+bool lineTracking = false;      // Will the robot track lines?
+bool obstacleAvoidance = true;  // Will the robot avoid obstacles?
+bool bluetoothControl = true;   // Will the robot accept bluetooth control? (WASD)
+bool infraredControl = false;   // Will the robot accept infrared control? (NOT RECCOMENDED)
+
+
+//
+
+bool tweenMotors = true;
+bool tweenTurning = true;
+
+
+
+//
